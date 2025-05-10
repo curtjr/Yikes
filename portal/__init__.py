0ef3ca3bff393c4ff402c5d66f041bbc502344dc
Intrepid <121172135+Intrepid999@users.noreply.github.com>
2025-05-09 22:12:51 -0400

    fixed stuff

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,6 +1,11 @@
-from portal.transport import Transport
+from portal.transport import Client
+from portal.transport import Server
 from portal.commands import CommandExecutor
 
 def connect(host):
-    transport = Transport(host)
-    return CommandExecutor(transport)
+    transport = Client(host)
+    return transport
+
+def start_server():
+    transport = Server()
+    return transport
