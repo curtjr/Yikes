7b310e1f901070884e89cd0deeb14050db4fc0b6
Intrepid <121172135+Intrepid999@users.noreply.github.com>
2025-07-25 00:20:07 -0400

    Added Extra Notes and Simplified JSON Sending

--- src/yikes/auth.py
+++ src/yikes/auth.py
@@ -1,19 +1,23 @@
-import jwt
-import base64
-import socket
-import threading
+import jwt # For adding tokens in the future
 import os
+from yikes.socks import send_key, receive_fernet_key, receive_public_key
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.fernet import Fernet
 
 class Authenticator:
     def new_client_key(self):
+        """
+            Generates a new fernet key for the client. This is automatically done when a client is instantiated.
+        """
         fernet_key = Fernet.generate_key()
         self.fernet = Fernet(fernet_key)
         self.encrypted_key = self.public_key.encrypt(fernet_key,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
 
     def new_server_keys(self):
+        """
+            Creates a new private and public key for the server. This is automatically done everytime there is a new client connection.
+        """
         self.private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         self.public_key = self.private_key.public_key()
 
@@ -26,13 +30,16 @@ class Authenticator:
             ))
 
     def client_handshake(self):
+        """
+            This is automatic.
+        """
         try:
             self.public_key = None
             while self.public_key is None:
-                self.public_key = self.receive_public_key(self.sock)
+                self.public_key = receive_public_key(self.sock)
             
             self.new_client_key()
-            self.send_key(self.sock, self.encrypted_key)
+            send_key(self.sock, self.encrypted_key)
             print("Client handshake successful")
             return True
         except Exception as e:
@@ -40,13 +47,15 @@ class Authenticator:
             return False
 
     def server_handshake(self, sock, addr):
-        # send your public key once
+        """
+            This is automatic.
+        """
         der_bytes = self.public_key.public_bytes(encoding=serialization.Encoding.DER,format=serialization.PublicFormat.SubjectPublicKeyInfo)
-        self.send_key(sock, der_bytes)
+        send_key(sock, der_bytes)
 
         key_bytes = None
         while key_bytes is None:
-            key_bytes = self.receive_fernet_key(sock)
+            key_bytes = receive_fernet_key(sock)
             
         fernet_key = self.private_key.decrypt(
             key_bytes,
--- src/yikes/socks.py
+++ src/yikes/socks.py
@@ -1,7 +1,8 @@
 import socket
 import struct
+import json
+import base64
 from cryptography.hazmat.primitives import serialization
-from cryptography.fernet import Fernet
 
 def recv_exact(sock: socket.socket, length: int):
     """Receive exactly 'length' bytes from a socket."""
@@ -14,34 +15,32 @@ def recv_exact(sock: socket.socket, length: int):
     return data
 
 class SocketHandler:
-    def send_key(self, sock: socket.socket, key_bytes: bytes):
-        try:
-            length = len(key_bytes)
-            header = struct.pack('>I', length)
-            sock.sendall(header + key_bytes)
-        except Exception as e:
-            print(f"Error sending key {e}")
+    def send_json(self, sock: socket.socket, json_obj):
+        """
+            Simplified version of send_data for sending jsons as base64 encoded bytes.
 
-    def receive_public_key(self, sock: socket.socket):
-        try:
-            header = recv_exact(sock, 4) # Get header (has length of key)
-            length, = struct.unpack(">I", header) # Length of key
+            Args:
+                sock(socket.socket): Target socket.
+                json_obj(json): Json object
+        """
+        json_str = json.dumps(json_obj)
+        json_bytes = base64.b64encode(json_str.encode())
+        self.send_data(sock, json_bytes)
 
-            der_bytes = recv_exact(sock, length) # Receive key
-            public_key = serialization.load_der_public_key(der_bytes) # Deserialize key
-            return public_key
-        except Exception as e:
-            print(f"Error receiving public key: {e}")
-    
-    def receive_fernet_key(self, sock: socket.socket):
-        try:
-            header = recv_exact(sock, 4)
-            length, = struct.unpack(">I", header)
+    def recv_json(self, sock: socket.socket):
+        """
+            Simplified version of recv_data for receiving jsons that are base64 encoded bytes.
 
-            key_bytes = recv_exact(sock, length)
-            return key_bytes
-        except Exception as e:
-            print(f"Error receiving fernet key {e}")
+            Args:
+                sock(socket.socket): Target socket.
+
+            Returns:
+                json_obj(json): Json object.
+        """
+        json_bytes = self.recv_data(sock)
+        json_str = base64.b64decode(json_bytes).decode()
+        json_obj = json.loads(json_str)
+        return json_obj
 
     def send_data(self, sock: socket.socket, data: bytes):
         """
@@ -59,7 +58,7 @@ class SocketHandler:
         except Exception as e:
             print(f"Error sending data: {e}")
 
-    def receive_data(self, sock: socket.socket):
+    def recv_data(self, sock: socket.socket):
         """
         Waits for and receives data from a socket, and decrypts the data using a Fernet.
 
@@ -79,3 +78,32 @@ class SocketHandler:
         except Exception as e:
             print(f"Error receiving data: {e}")
 
+def send_key(sock: socket.socket, key_bytes: bytes):
+    try:
+        length = len(key_bytes)
+        header = struct.pack('>I', length)
+        sock.sendall(header + key_bytes)
+    except Exception as e:
+        print(f"Error sending key {e}")
+
+def receive_public_key(sock: socket.socket):
+    try:
+        header = recv_exact(sock, 4) # Get header (has length of key)
+        length, = struct.unpack(">I", header) # Length of key
+
+        der_bytes = recv_exact(sock, length) # Receive key
+        public_key = serialization.load_der_public_key(der_bytes) # Deserialize key
+        return public_key
+    except Exception as e:
+        print(f"Error receiving public key: {e}")
+    
+def receive_fernet_key(sock: socket.socket):
+    try:
+        header = recv_exact(sock, 4)
+        length, = struct.unpack(">I", header)
+
+        key_bytes = recv_exact(sock, length)
+        return key_bytes
+    except Exception as e:
+        print(f"Error receiving fernet key {e}")
+    
